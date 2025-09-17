12234f17b631143c69c276bb91b7d968623ae922
Velichko Slavilov <velichkoslavilov98@gmail.com>
2025-09-17 10:40:24 +0300

    Create 2.InsertingData.sql
    2) Inserting Data

---
+++ 1. SQL Introduction/2.InsertingData.sql
@@ -0,0 +1,10 @@
+INSERT INTO dbo.Students (StudentID, FirstName, LastName, Age, Grade) VALUES
+(1, 'John',     'Doe',       15, '10th'),
+(2, 'Jane',     'Smith',     16, '11th'),
+(3, 'Michael',  'Johnson',   17, '12th'),
+(4, 'Emily',    'Brown',     14, '9th'),
+(5, 'David',    'Williams',  16, '11th'),
+(6, 'Sophia',   'Miller',    15, '10th'),
+(7, 'Daniel',   'Davis',     18, '12th'),
+(8, 'Olivia',   'Garcia',    14, '9th');
+GO
