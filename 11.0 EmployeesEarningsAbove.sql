b20706d98bbe705e29d29b263288243133975f1a
Velichko Slavilov <velichkoslavilov98@gmail.com>
2025-10-12 22:15:54 +0300

    Delete 11.0 EmployeesEarningsAbove.sql

--- 11.0 EmployeesEarningsAbove.sql
+++
@@ -1,8 +0,0 @@
-WITH avg_cte AS (SELECT AVG(Salary) AS avg_salary FROM dbo.Employees)
-SELECT CONCAT(e.FirstName, N' ', e.LastName) AS FullName,
-       e.JobTitle,
-       e.Salary
-FROM dbo.Employees e
-CROSS JOIN avg_cte a
-WHERE e.Salary > a.avg_salary
-ORDER BY e.Salary DESC;
